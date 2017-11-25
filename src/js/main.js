edcfb6f3f67a5a134f341f032dd7b9a1d36013cc
Pawel Sroczynski <p.sroczynski@gmail.com>
2017-11-25 22:24:50 +0100

    Optimized main menu transitions when scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 $(window).scroll(function(){
     if($(this).scrollTop() == 0){
-        $('nav').removeClass('shrink',1000);
+        $('header nav').removeClass('shrink',1000);
+        $('header nav').addClass('top',1000, "easeOutSine");
     }
     else {
-        $('nav').addClass('shrink',1000, "easeOutSine");
+        $('header nav').addClass('shrink',1000, "easeOutSine");
+        $('header nav').removeClass('top',1000);
     }
 });
