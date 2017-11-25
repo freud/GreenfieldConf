c8e4f152d6ff5d3bb70e795fae67210e0d12ef41
Pawel Sroczynski <p.sroczynski@gmail.com>
2017-11-25 12:05:31 +0100

    Change output css files and js build directory

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,13 +10,13 @@ let mix = require('laravel-mix');
  | file for your application, as well as bundling up your JS files.
  |
  */
-mix.setPublicPath('public/assets/build');
+mix.setPublicPath('public/');
 mix.js([
         'node_modules/jquery/dist/jquery.js',
         'node_modules/popper.js/dist/popper.js',
         'node_modules/bootstrap/dist/js/bootstrap.js',
         'src/js/main.js'
-    ], 'public/assets/build/js/main.js');
-mix.sass('src/scss/main.scss', 'public/assets/build/css/main.css');
-mix.copyDirectory('src/res/', 'public/assets/build/res/');
+    ], 'main.js');
+mix.sass('src/scss/main.scss', 'main.css');
+mix.copyDirectory('src/res/', 'res/');
 
