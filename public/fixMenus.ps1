a480ed24670facef8b5d6643332526b8a3fc592c
gabr <arkadiuszgabrys@gmail.com>
2018-01-14 15:48:51 +0100

    Type in fixMenus

--- public/fixMenus.ps1
+++ public/fixMenus.ps1
@@ -2,7 +2,8 @@ $patterns = @(
     @{ name = "Agenda";      address = "schedule.html" },
     @{ name = "Prelegenci";  address = "speakers.html" },
     @{ name = "Partnerzy";   address = "partners.html" },
-    @{ name = "Lokalizacja"; address = "locatoin.html" },
+    @{ name = "Lokalizacja"; address = "location.html" },
+    @{ name = "Kontakt"; address = "location.html" },
     @{ name = "Aktualnoœci"; address = "news.html" },
     @{ name = "Kup bilet";   address = "buy-ticket.html" }
 )
@@ -12,7 +13,7 @@ dir *.html -name | foreach {
     $lines = Get-Content $_ -Encoding UTF8
     for ($l = 0; $l -lt $lines.Count; $l++) {
         foreach ($p in $patterns) {
-            if ($lines[$l] -match ('<a.*?(href=.#.).*?>' + $p.name + '<[\/]a>')) {
+            if ($lines[$l] -match ('<a.*?(href=[''"].+?[''"]).*?>' + $p.name + '<[\/]a>')) {
                 Write-Verbose "Found link to fix: $($lines[$l])" -Verbose
                 $lines[$l] = $lines[$l].Replace($Matches[1], "href=`"$($p.address)`"")
                 Write-Verbose "Fixed: $($lines[$l])" -Verbose
