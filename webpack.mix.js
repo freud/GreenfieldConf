1b221794f7f52d8b623933b6146798241c692e98
Paweł Sroczyński <p.sroczynski@gmail.com>
2018-04-30 11:11:59 +0200

    Add slides reordering, so the first slides is always different!

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,6 +19,7 @@ mix.js([
         'node_modules/jquery/dist/jquery.slim.js',
         'node_modules/popper.js/dist/umd/popper.js',
         'node_modules/bootstrap/dist/js/bootstrap.js',
+        'node_modules/jquery.cookie/jquery.cookie.js',
         'src/js/map.js',
         'src/js/main.js'
     ], 'main.js');
