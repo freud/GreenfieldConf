4296069f73c1cf02afb4e7e777715e11bd02b7ee
BIG EYE <bigeyedes@gmail.com>
2019-02-03 14:56:31 +0100

    Counter

--- src/js/main.js
+++ src/js/main.js
@@ -56,3 +56,23 @@ $(document).ready(function(){
 		pause: "false"
 	})
 })
+
+//Ticket counter
+$(document).ready(function(){
+	const second = 1000,
+	minute = second * 60,
+	hour = minute * 60,
+	day = hour * 24;
+
+let countDown = new Date('Jan 18, 2019 12:00:00').getTime(),
+	x = setInterval(function() {
+
+		let now = new Date().getTime(),
+			distance = countDown - now;
+
+		document.getElementById('days').innerHTML = Math.floor(distance / (day)),
+			document.getElementById('hours').innerHTML = Math.floor((distance % (day)) / (hour)),
+			document.getElementById('minutes').innerHTML = Math.floor((distance % (hour)) / (minute)),
+			document.getElementById('seconds').innerHTML = Math.floor((distance % (minute)) / second);
+	}, second)
+})
