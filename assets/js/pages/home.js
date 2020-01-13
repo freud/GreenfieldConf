e8d780c39e4035c5807097d3ed5213d13693f9be
khypki <krhypki@gmail.com>
2020-01-13 19:25:20 +0100

    Home partners swiper

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -1,21 +1,48 @@
 window.onload = () => {
-    const speakerSwiper = new Swiper('#swiper-speakers', {
-        speed: 400,
-        loop: true,
-        init: false,
-        slidesPerPage: 1,
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            bulletElement: 'button',
-            clickable: true
+
+
+    // Uncomment if there is more than one partner
+    // createSwiper({
+    //     selector: '#swiper-partners',
+    //     props: {
+    //         spaceBetween: 60
+    //     }
+    // });
+
+    createSwiper({
+        selector: '#swiper-speakers', 
+        props: { init: false },
+        initFunc: () => {
+            speakerModal.init();
         }
     });
 
-    speakerSwiper.on('init', function() {
-        speakerModal.init();
-    });
+    function createSwiper({ selector, props = {}, initFunc }) {
+        const { speed, loop, init, slidesPerPage, pagination, spaceBetween} = props;
 
-    speakerSwiper.init();
+        const newSwiper =  new Swiper(selector, {
+            speed: speed || 400,
+            loop: typeof loop !== 'undefined' ? loop : true,
+            init: typeof init !== 'undefined' ? init : true,
+            spaceBetween: spaceBetween || 0,
+            slidesPerPage: slidesPerPage || 1,
+            pagination: pagination || {
+                el: '.swiper-pagination',
+                type: 'bullets',
+                bulletElement: 'button',
+                clickable: true
+            }
+        }); 
+
+        if (init) {
+            return newSwiper;
+        }
+
+        newSwiper.on('init', () => {
+            initFunc();
+        });
+
+        newSwiper.init();
+    }
+
 }
-
